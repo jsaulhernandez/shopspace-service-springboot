308f7fb27e056cbf34dc39a27564fe86b9ba9dcf
Sony <jsaulhernandez98@gmail.com>
2023-07-04 18:20:06 -0600

    tag: adding logger to category impl

--- src/main/java/com/shopspace/shopspaceservice/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/shopspace/shopspaceservice/service/impl/CategoryServiceImpl.java
@@ -3,6 +3,8 @@ package com.shopspace.shopspaceservice.service.impl;
 import com.shopspace.shopspaceservice.model.Category;
 import com.shopspace.shopspaceservice.repository.CategoryRepository;
 import com.shopspace.shopspaceservice.service.CategoryService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -13,6 +15,7 @@ import java.util.Optional;
 
 @Service
 public class CategoryServiceImpl implements CategoryService {
+    Logger logger = LoggerFactory.getLogger(CategoryServiceImpl.class);
 
     @Autowired
     private CategoryRepository categoryRepository;
@@ -38,7 +41,7 @@ public class CategoryServiceImpl implements CategoryService {
             categoryRepository.deleteById(id);
             return true;
         }catch (Exception e){
-            System.out.println("Exception {}" + e);
+            logger.error("Exception {0}", e);
             return false;
         }
     }
