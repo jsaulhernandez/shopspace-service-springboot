b1aec2a8799fc1f3811a700b3866f8b67ad4d3c0
Sony <jsaulhernandez98@gmail.com>
2023-08-17 10:26:52 -0600

    tag: reformatting code to category repository

--- src/main/java/com/shopspace/shopspaceservice/repository/CategoriesRepository.java
+++ src/main/java/com/shopspace/shopspaceservice/repository/CategoriesRepository.java
@@ -7,7 +7,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+
 public interface CategoriesRepository extends JpaRepository<Categories, Long> {
     @Query("SELECT c FROM Categories c WHERE (:search IS NULL or c.name LIKE CONCAT('%', :search, '%')) ORDER BY c.id DESC")
     Page<Categories> getAllCategories(@Param("search") String Search, Pageable pageable);
+
+    // Methods for web
+    List<Categories> findAllByStatus(Integer status);
 }
